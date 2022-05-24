be2e0ed7794dbd12d520518513744d82b2b996dc
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2022-05-25 01:46:33 +0900

    loading画面の追加

--- js/main.js
+++ js/main.js
@@ -1,6 +1,46 @@
 "use strict"
 
-// ↓↓ TOPへ戻るボタンの表示 ↓↓ 
+// ↓↓ loadingアニメーション ↓↓ ---------------
+
+  const loading_img = document.getElementsByClassName("loading__img");
+
+  let anime_count = 0;
+
+  function load_anime(){
+    loading_img[anime_count].classList.add("show");
+    for(let i = 0;i < loading_img.length;i++){
+      if(i == anime_count){
+        continue;
+      };
+      loading_img[i].classList.remove("show");
+    };
+    anime_count++;
+    if(anime_count >= loading_img.length){
+      anime_count = 0;
+    };
+  };
+
+  setInterval(load_anime,500);
+
+// ↑↑ loadingアニメーション ↑↑ ---------------
+
+
+// ↓↓ loading画面の終了 ↓↓ ---------------
+
+  const loading = document.getElementById("loading");
+
+  function loaded(){
+    loading.classList.add("loaded");
+  };
+
+  window.addEventListener("load",loaded);
+  // setTimeout(loaded,2000);
+
+// ↑↑ loading画面の終了 ↑↑ ---------------
+
+
+
+// ↓↓ TOPへ戻るボタンの表示 ↓↓ ---------------
 
   const target = document.getElementById("target");
   const to_top = document.getElementById("to_top");
@@ -17,10 +57,10 @@
   // スクロールするたびに判定
   window.addEventListener("scroll",top_button);
 
-// ↑↑ TOPへ戻るボタンの表示 ↑↑ 
+// ↑↑ TOPへ戻るボタンの表示 ↑↑ ---------------
 
 
-// ↓↓ hero__listの横移動 ↓↓ 
+// ↓↓ hero__listの横移動 ↓↓ ---------------
 
   const hero_lists = document.getElementsByClassName("hero__list");
 
@@ -29,18 +69,19 @@
 
       const y = hero_lists[i].getBoundingClientRect().y;
       const point = 240;
+      const move = y - point;
 
       if(i%2){
         if(y < point){
-          hero_lists[i].style.transform = `translateX(${(y - point) * (-1)}px)`;
+          hero_lists[i].style.transform = `translate(${-move}px,${-move / 2}px)`;
         } else {
-          hero_lists[i].style.transform = "translateX(0)";
+          hero_lists[i].style.transform = "translate(0)";
         };
       } else {
         if(y < point){
-          hero_lists[i].style.transform = `translateX(${y - point}px)`;
+          hero_lists[i].style.transform = `translate(${move}px,${-move / 2}px)`;
         } else {
-          hero_lists[i].style.transform = "translateX(0)";
+          hero_lists[i].style.transform = "translate(0)";
         };
       };
     };
@@ -49,6 +90,6 @@
   // スクロールするたびに判定
   window.addEventListener("scroll",to_side);
 
-// ↑↑ hero__listの横移動 ↑↑ 
+// ↑↑ hero__listの横移動 ↑↑ ---------------
 
 
