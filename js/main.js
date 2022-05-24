287c177d8f1c128db4813aa3fd0602ccfffb7ef4
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2022-05-24 19:34:11 +0900

    jsのリファクタリング

--- js/main.js
+++ js/main.js
@@ -1,85 +1,54 @@
 "use strict"
 
+// ↓↓ TOPへ戻るボタンの表示 ↓↓ 
 
-//hero__listの横移動----------------------------------------
+  const target = document.getElementById("target");
+  const to_top = document.getElementById("to_top");
 
-const hero_lists = document.getElementsByClassName("hero__list")
-
-// スクロールするたびに判定
-window.addEventListener("scroll",to_side);
-
-function to_side(){
-  for(let i = 0;i < hero_lists.length;i++){
-    const y = hero_lists[i].getBoundingClientRect().y;
-    if(i%2){
-      if(y < 250){
-        hero_lists[i].style.transform = `translateX(${(y - 250) * (-1)}px)`;
-      } else {
-        hero_lists[i].style.transform = "translateX(0)";
-      };
+  // 位置を確認してクラスを制御
+  function top_button(){
+    if(target.getBoundingClientRect().y < -100){
+      to_top.classList.add("show");
     } else {
-      if(y < 250){
-        hero_lists[i].style.transform = `translateX(${y - 250}px)`;
+      to_top.classList.remove("show");
+    };
+  };
+
+  // スクロールするたびに判定
+  window.addEventListener("scroll",top_button);
+
+// ↑↑ TOPへ戻るボタンの表示 ↑↑ 
+
+
+// ↓↓ hero__listの横移動 ↓↓ 
+
+  const hero_lists = document.getElementsByClassName("hero__list");
+
+  function to_side(){
+    for(let i = 0;i < hero_lists.length;i++){
+
+      const y = hero_lists[i].getBoundingClientRect().y;
+      const point = 240;
+
+      if(i%2){
+        if(y < point){
+          hero_lists[i].style.transform = `translateX(${(y - point) * (-1)}px)`;
+        } else {
+          hero_lists[i].style.transform = "translateX(0)";
+        };
       } else {
-        hero_lists[i].style.transform = "translateX(0)";
+        if(y < point){
+          hero_lists[i].style.transform = `translateX(${y - point}px)`;
+        } else {
+          hero_lists[i].style.transform = "translateX(0)";
+        };
       };
     };
   };
-};
+
+  // スクロールするたびに判定
+  window.addEventListener("scroll",to_side);
+
+// ↑↑ hero__listの横移動 ↑↑ 
 
 
-
-
-//SKILLの遅延表示----------------------------------------
-const lists = document.querySelectorAll("li");
-
-function callback(entries,obj){
-  entries.forEach(entry=>{
-
-    if(!entry.isIntersecting){
-      return;
-    };
-
-    entry.target.classList.add("appear");
-    obj.unobserve(entry.target);
-  });
-};
-
-const option = {
-  threshold : .5,
-};
-
-const observer = new IntersectionObserver(callback,option);
-
-lists.forEach(li=>{
-  observer.observe(li);
-});
-
-
-//TOPボタンの遅延表示---------------------------------------
-const target = document.getElementById("target");
-const header = document.querySelector("header");
-const toTop = document.getElementById("top");
-
-function callback2(e){
-  if(!e[0].isIntersecting){
-    toTop.classList.add("scrolled");
-    header.classList.add("scrolled");
-  } else {
-    toTop.classList.remove("scrolled");
-    header.classList.remove("scrolled");
-  };
-};
-
-const observer2 = new IntersectionObserver(callback2);
-
-toTop.addEventListener("click",e =>{
-  e.preventDefault();
-
-  window.scrollTo({
-    top:0,
-    behavior:"smooth",
-  });
-});
-
-observer2.observe(target);
